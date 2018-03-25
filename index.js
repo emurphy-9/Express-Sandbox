f67d87403ba67cabfd201ff76347860da4646196
Eoin Murphy <eoinmurphy95@gmail.com>
2018-03-25 14:46:02 +0100

    Text change to check connection to remote

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 	res.append("X-Custom-Header", "My custom value!");
-	res.send('Hello World!');
+	res.send('Hello World!\nMore content coming soon...');
 });
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
